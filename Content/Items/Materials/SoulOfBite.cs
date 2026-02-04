29668a233c3b7181a36ef055f9d206577a04c220
Dynamic-Pistol <spidermalek666@gmail.com>
2026-02-05 00:14:15 +0200

    Updated souls

--- Content/Items/Materials/SoulOfBite.cs
+++ Content/Items/Materials/SoulOfBite.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -11,12 +12,23 @@ namespace aylocks.Content.Items.Materials
         public override void SetStaticDefaults()
         {
             Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
+			ItemID.Sets.ItemIconPulse[Type] = true; // The item pulses while in the player's inventory
+			ItemID.Sets.ItemNoGravity[Type] = true; // Makes the item have no gravity
         }
 
         public override void SetDefaults()
         {
             Item.maxStack = Item.CommonMaxStack; // The item's max stack value
             Item.value = Item.sellPrice(silver: 5, copper: 20);
+            
         }
+
+		public override void PostUpdate() {
+			Lighting.AddLight(Item.Center, Color.WhiteSmoke.ToVector3() * 0.55f * Main.essScale); // Makes this item glow when thrown out of inventory.
+		}
+
+		public override Color? GetAlpha(Color lightColor) {
+			return new Color(0, 255, 0, 50); // Makes this item render at full brightness.
+		}
     }
 }
